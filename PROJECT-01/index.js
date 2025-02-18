1b75469e88a3ed4d1f25f8305e1d6fd7b082d526
Devanshu046 <dev44908@gmail.com>
2025-02-18 22:49:43 +0530

    Basic REST CRUD ROUTES

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,7 +1,42 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 const app = express();
 
 const PORT = 8000;
+//ROUTES
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+  res.send(html);
+});
 
+// REST API END POINTS
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
 
-app.listen(PORT,() => console.log(`Server started at PORT ${PORT}`));
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // TODO Update The User With id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // TODO Delete The User With id
+    return res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  //TODO Create New User
+  return res.json({ status: "pending" });
+});
+
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
