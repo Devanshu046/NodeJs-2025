8ef564664f546b2210641576ac51233b44429f37
Devanshu046 <dev44908@gmail.com>
2025-02-20 23:23:53 +0530

    CRUD Completed

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 const app = express();
-
 const PORT = 8000;
+
+app.use(express.urlencoded({extended: false}))
 //ROUTES
 app.get("/users", (req, res) => {
   const html = `
@@ -26,17 +28,30 @@ app
     return res.json(user);
   })
   .patch((req, res) => {
+    const userIndex = users.findIndex((user) => user.id === Number(req.params.id));
+    users[userIndex] = { ...users[userIndex], ...req.body };
     // TODO Update The User With id
-    return res.json({ status: "pending" });
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err ,data)=>{
+      return res.json({ status: "sucess",id: Number(req.params.id) });
+    })
   })
   .delete((req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+    users.splice(userIndex,1);
     // TODO Delete The User With id
-    return res.json({ status: "pending" });
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err ,data)=>{
+      return res.json({ status: "sucess",id:id});
+    })
+   
   });
 
 app.post("/api/users", (req, res) => {
-  //TODO Create New User
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({...body,id: users.length+1});
+  fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err ,data)=>{
+    return res.json({ status: "success",id: users.length });
+  })
 });
 
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
